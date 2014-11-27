ecb0add9dc373ca9234c7f90803ab1ae08da1bf8
Marcel Ngan <marcel82792@gmail.com>
2014-11-27 04:53:38 +0000

    non tested implementatino of SSA, please check

--- SRTool/srtool/tool/src/srt/tool/SSAVisitor.java
+++ SRTool/srtool/tool/src/srt/tool/SSAVisitor.java
@@ -1,5 +1,8 @@
 package srt.tool;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import srt.ast.AssignStmt;
 import srt.ast.Decl;
 import srt.ast.DeclRef;
@@ -7,23 +10,35 @@ import srt.ast.visitor.impl.DefaultVisitor;
 
 public class SSAVisitor extends DefaultVisitor {
 
+	private Map<String, Integer> index = new HashMap<String, Integer>();
+
 	public SSAVisitor() {
 		super(true);
 	}
 
 	@Override
 	public Object visit(Decl decl) {
-		return super.visit(decl);
+		String name = decl.getName();
+		index.put(name, 0);
+		String SSAName = name+"$"+index.get(name);
+		return new Decl(SSAName, decl.getType(), decl.getnodeInfo());
 	}
 
 	@Override
 	public Object visit(DeclRef declRef) {
-		return super.visit(declRef);
+		String name = declRef.getName();
+		String SSAName = name+"$"+index.get(name);
+		return new DeclRef(SSAName, declRef.getnodeInfo());
 	}
 
 	@Override
 	public Object visit(AssignStmt assignment) {
-		return super.visit(assignment);
+		Expr expr  = (Expr) super.visit(assignment.getRhs());
+		String lhsName = assignment.getLhs().getName();
+		int i = index.get(lhsName) + 1;
+        index.put(name, i);
+		DeclRef declRef  = (DeclRef) this.visit(assignment.getLhs());
+		return new AssignStmt(declRef, expr, assignment);
 	}
 
 }
